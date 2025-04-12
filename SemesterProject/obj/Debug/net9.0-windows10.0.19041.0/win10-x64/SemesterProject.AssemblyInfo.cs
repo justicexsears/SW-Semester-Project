310181df1c4e1b0e36b0aef4a9a7601d9e8f94ad
justicexsears <w10110632@usm.edu>
2025-04-12 17:13:33 -0500

    Preview Image

--- SemesterProject/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SemesterProject.AssemblyInfo.cs
+++ SemesterProject/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SemesterProject.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemesterProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+07dd11b9029a96f1d2cf21376fb5af138a6220e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+db0e161655731b2446cc35126993196b2ae22955")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemesterProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemesterProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
